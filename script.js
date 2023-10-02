52e6c9d2b65a09303686ef2dce2caa659ade02b8
Vinicius da Costa Souza <viniciuscsouza@yahoo.com.br>
2023-10-02 20:56:29 -0300

    adiciona Materialize CSS

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ function criaElementoTarefaNaListaDeTarefas(parametroNomeDaTarefa){
     
     const elementoBotaoEditarTarefa = document.createElement('button')
     elementoBotaoEditarTarefa.innerText = "Editar"
+    elementoBotaoEditarTarefa.className = "waves-effect waves-light btn-small"
     elementoBotaoEditarTarefa.addEventListener("click", editaElementoTarefaNaListaDeTarefas)
     
     const elementoBotaoExcluirTarefa = document.createElement('button')
     elementoBotaoExcluirTarefa.innerText = "Excluir"
+    elementoBotaoExcluirTarefa.className = "waves-effect waves-light btn-small red darken-2"
     elementoBotaoExcluirTarefa.addEventListener("click", removeElementoTarefaNaListaDeTarefas)
   
     const elementoDivTarefa = document.createElement('div')
@@ -42,7 +44,7 @@ function editaElementoTarefaNaListaDeTarefas(objetoDeEventos){
     const botaoQueFoiClicado = objetoDeEventos.target
     const paragrafoDaTarefa = botaoQueFoiClicado.previousSibling
     const textoInternoDoParagrafo = paragrafoDaTarefa.innerText
+    botaoQueFoiClicado.parentNode.remove()
     const elementoInputTarefa = document.getElementById('input-tarefa')
     elementoInputTarefa.value = textoInternoDoParagrafo
-    botaoQueFoiClicado.parentNode.remove()
-}
+}   elementoInputTarefa.focus()
